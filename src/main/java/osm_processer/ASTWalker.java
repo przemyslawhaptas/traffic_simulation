ed07652434998d978ddea2483ce699cb8e67d300
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-06-06 21:25:29 +0200

    Add osm_processor module responsible for parsing osm

---
+++ src/main/java/osm_processer/ASTWalker.java
@@ -0,0 +1,7 @@
+package osm_processer;
+
+/**
+ * Created by admin on 06/06/16.
+ */
+public class ASTWalker {
+}
---
+++ src/main/java/osm_processer/Main.java
@@ -0,0 +1,29 @@
+package osm_processer;
+
+import org.antlr.runtime.*;
+import org.antlr.runtime.tree.*;
+
+public class Main {
+    public static int minLat;
+    public static int minLon;
+    public static int maxLat;
+    public static int maxLon;
+
+    public static void main(String[] args) {
+        try {
+            CharStream input = new ANTLRFileStream(args[0]);
+            OSMLexer lex = new OSMLexer(input);
+
+            CommonTokenStream tokens = new CommonTokenStream(lex);
+            OSMParser parser = new OSMParser(tokens);
+            OSMParser.document_return root = parser.document();
+            CommonTree root_tree = (CommonTree) root.tree;
+
+            ASTWalker walker = new ASTWalker(root_tree);
+            walker.walkTree(root_tree, 0);
+        } catch (Throwable t) {
+            System.out.println("exception: " + t);
+            t.printStackTrace();
+        }
+    }
+}

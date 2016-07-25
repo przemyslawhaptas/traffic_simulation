5961d49c71c2a2b8080c5086d3b88f61a9265d55
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-07-25 15:22:24 +0200

    Refactor main function

--- src/main/java/aparapi/ArrayOps.java
+++ src/main/java/aparapi/ArrayOps.java
@@ -15,40 +15,10 @@ public class ArrayOps {
 
     public static void main(String[] args) {
         int[] startTraffic = initializeTraffic();
-        Random random = new Random();
-        long seed = random.nextLong();
+        TrafficModel trafficModel = buildTrafficModel(startTraffic);
+        int streetsNumber = countStreets(trafficModel);
 
-        TrafficModel trafficModel = new TrafficModel(startTraffic, STREETS_CELLS_SIZE, seed);
-        int[] traffic = trafficModel.getTraffic();
-        int streetsNumber = traffic.length / STREETS_CELLS_SIZE;
-
-        System.out.println("cap=cars=outputs==tries=destination\n");
-        for (int i = 0; i < ITERATIONS_NUMBER; i++) {
-            System.out.println("TURN " + i + " ==========================================");
-            trafficModel.execute(streetsNumber);
-            if (!trafficModel.getExecutionMode().equals(Kernel.EXECUTION_MODE.GPU)){
-                System.out.println("Kernel did not execute on the GPU!");
-            }
-
-            printTraffic(trafficModel.getTraffic(), streetsNumber);
-        }
-    }
-
-    public static void printTraffic(int[] traffic, int streetsNumber) {
-
-
-        for (int i = 0; i < streetsNumber; i++) {
-            int[] street = Arrays.copyOfRange(traffic, i * STREETS_CELLS_SIZE, (i + 1) * STREETS_CELLS_SIZE);
-            printStreet(street);
-        }
-    }
-
-    public static void printStreet(int[] street) {
-        System.out.print("{ ");
-        for(int i = 0; i < street.length; i++) {
-            System.out.print(street[i] + " ");
-        }
-        System.out.print("}\n");
+        run(trafficModel, streetsNumber);
     }
 
     public static int[] initializeTraffic() {
@@ -62,5 +32,46 @@ public class ArrayOps {
                 5, 5, 1, 6, 1, 3, 0, 0, 0, -1,
                 5, 3, 1, 0, 2, 0, 0, 0, 0, -1,
                 5, 1, 2, 1, 3, 0, 0, 0, 0, -1};
+    }
+
+    private static TrafficModel buildTrafficModel(int[] startTraffic) {
+        Random random = new Random();
+        long seed = random.nextLong();
+
+        return new TrafficModel(startTraffic, STREETS_CELLS_SIZE, seed);
+    }
+
+    public static void run(TrafficModel trafficModel, int streetsNumber) {
+        System.out.println("cap=cars=outputs==tries=destination\n");
+        for (int i = 0; i < ITERATIONS_NUMBER; i++) {
+            System.out.println("TURN " + i + " ==========================================");
+            trafficModel.execute(streetsNumber);
+            if (!trafficModel.getExecutionMode().equals(Kernel.EXECUTION_MODE.GPU)){
+                System.out.println("Kernel did not execute on the GPU!");
+            }
+
+            printTraffic(trafficModel.getTraffic(), streetsNumber);
+        }
+    }
+
+    private static int countStreets(TrafficModel trafficModel) {
+        int[] traffic = trafficModel.getTraffic();
+
+        return traffic.length / STREETS_CELLS_SIZE;
+    }
+
+    private static void printTraffic(int[] traffic, int streetsNumber) {
+        for (int i = 0; i < streetsNumber; i++) {
+            int[] street = Arrays.copyOfRange(traffic, i * STREETS_CELLS_SIZE, (i + 1) * STREETS_CELLS_SIZE);
+            printStreet(street);
+        }
+    }
+
+    private static void printStreet(int[] street) {
+        System.out.print("{ ");
+        for(int i = 0; i < street.length; i++) {
+            System.out.print(street[i] + " ");
+        }
+        System.out.print("}\n");
     }
 }
